c3c33cc4737639162e6795be587ca66fc6c5483d
RodBrowning <rodrigo.browning@gmail.com>
2022-08-01 17:45:30 -0300

    Set large screen grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,26 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() => setCount((oldCount) => oldCount + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header>
+        <div className="inner-header">
+          <div className="logo"></div>
+          <nav></nav>
+        </div>
       </header>
+      <div className="panel">
+        <div className="inner-panel">
+          <aside></aside>
+          <main></main>
+        </div>
+      </div>
+      <footer>
+        <div className="copyright"></div>
+        <div className="social-media"></div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
