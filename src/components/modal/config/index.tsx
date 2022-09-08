06fcfc604782a9fa6c55d3002f25863709b7bd54
RodBrowning <rodrigo.browning@gmail.com>
2022-09-08 18:00:07 -0300

    Set style reaction for config buttons

--- src/components/modal/config/index.tsx
+++ src/components/modal/config/index.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable sort-imports */
 import './style.scss';
 import './style-mobile.scss';
 
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { resetGame, generateNewBoardData } from '../../../features/game/gameSlice';
-import ConfirmationModal from '../confirmation/confirmationModal';
+import { generateNewBoardData, resetGame } from '../../../features/game/gameSlice';
+
 import ConfirmationComponent from '../confirmation/confirmationComponent';
+import ConfirmationModal from '../confirmation/confirmationModal';
+// eslint-disable-next-line import/order
+import { useDispatch } from 'react-redux';
 
 interface Props {
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -25,17 +26,27 @@ const ConfigModal: React.FC<Props> = ({ setOpenModal }) => {
     <div className="config-panel show-config">
       <div className="level">
         <h5>Dificuldade</h5>
-        <button type="button">Fácil</button>
-        <button type="button" className="selected">
-          Médio
-        </button>
-        <button type="button">Difícil</button>
+        <label htmlFor="easy">
+          <input type="radio" name="difficulty-radio" id="easy" />
+          <button type="button">Fácil</button>
+        </label>
+        <label htmlFor="normal">
+          <input type="radio" name="difficulty-radio" id="normal" />
+          <button type="button">Médio</button>
+        </label>
+        <label htmlFor="hard">
+          <input type="radio" name="difficulty-radio" id="hard" />
+          <button type="button">Difícil</button>
+        </label>
       </div>
       <div className="word-list">
         <h5>Lista de palavras</h5>
-        <button type="button" className="toggle-button">
-          Usar Custom
-        </button>
+        <label htmlFor="use-custom">
+          <input type="checkbox" name="custom-checkbox" id="use-custom" />
+          <button type="button" className="toggle-button">
+            Usar Custom
+          </button>
+        </label>
         <p>Separe com espaço ou virgula.</p>
         <p>Mínimo 10 palavras.</p>
         <textarea name="word-list" id="word-list" cols={10} rows={8} />
@@ -43,16 +54,38 @@ const ConfigModal: React.FC<Props> = ({ setOpenModal }) => {
       <div className="subject">
         <h5>Tema</h5>
         <div className="options">
-          <button type="button">Animais</button>
-          <button type="button" className="selected">
-            Carros
-          </button>
-          <button type="button">Esportes</button>
-          <button type="button">Nomes</button>
-          <button type="button">Bíblicos</button>
-          <button type="button">Peixes</button>
-          <button type="button">Marcas</button>
-          <button type="button">Aves</button>
+          <label htmlFor="animals">
+            <input type="checkbox" name="custom-checkbox" id="animals" />
+            <button type="button">Animais</button>
+          </label>
+          <label htmlFor="cars">
+            <input type="checkbox" name="custom-checkbox" id="cars" />
+            <button type="button">Carros</button>
+          </label>
+          <label htmlFor="sports">
+            <input type="checkbox" name="custom-checkbox" id="sports" />
+            <button type="button">Esportes</button>
+          </label>
+          <label htmlFor="names">
+            <input type="checkbox" name="custom-checkbox" id="names" />
+            <button type="button">Nomes</button>
+          </label>
+          <label htmlFor="bible">
+            <input type="checkbox" name="custom-checkbox" id="bible" />
+            <button type="button">Bíblicos</button>
+          </label>
+          <label htmlFor="fishes">
+            <input type="checkbox" name="custom-checkbox" id="fishes" />
+            <button type="button">Peixes</button>
+          </label>
+          <label htmlFor="companies">
+            <input type="checkbox" name="custom-checkbox" id="companies" />
+            <button type="button">Marcas</button>
+          </label>
+          <label htmlFor="birds">
+            <input type="checkbox" name="custom-checkbox" id="birds" />
+            <button type="button">Aves</button>
+          </label>
         </div>
       </div>
       <div className="reset-game">
