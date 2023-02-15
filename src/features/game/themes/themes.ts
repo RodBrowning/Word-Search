a075a18854fa0c962b304dfe42405e9657c4e289
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-02-15 16:23:35 -0300

    Rearrange themes list

--- src/features/game/themes/themes.ts
+++ src/features/game/themes/themes.ts
@@ -1,21 +1,23 @@
 import Animals from './animals';
-import Aquatic from './aquatic';
 import Bible from './bible';
 import Birds from './birds';
 import Cars from './cars';
 import Companies from './companies';
+import Land from './land';
+import Marine from './marine';
 import Names from './names';
-import sports from './sports';
+import Sports from './sports';
 
 const Themes = {
-  ...Animals(),
-  ...Cars(),
-  ...Bible(),
-  ...Birds(),
-  ...Companies(),
-  ...Aquatic(),
   ...Names(),
-  ...sports(),
+  ...Sports(),
+  ...Animals(),
+  ...Land(),
+  ...Birds(),
+  ...Marine(),
+  ...Bible(),
+  ...Cars(),
+  ...Companies(),
 };
 
 export default Themes;
