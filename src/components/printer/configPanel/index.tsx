ec58af06c15f2b6026a0d366842dfb2a99c82b65
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-02-27 15:35:03 -0300

    Move printer config panel to a component

---
+++ src/components/printer/configPanel/index.tsx
@@ -0,0 +1,220 @@
+import './style.scss';
+import './style-mobile.scss';
+
+import React, { useLayoutEffect, useState } from 'react';
+
+import CustomWordListConfig from '../../configCustomWordList';
+import IFeedback from '../../../types/feedback';
+import RangeInputComponent from '../../rangeInputComponent';
+import { RootState } from '../../../app/store';
+import SearchWordsGame from '../../../utils/SearchWordGame';
+import ThemesSelector from '../../configThemesSelector';
+import { useSelector } from 'react-redux';
+import useSessionStorage from '../../../utils/customHooks/useSessionStorage';
+
+interface Props {
+  handlePrint: () => void;
+  setBoardsToPrintArray: (
+    boardsToPrint: {
+      board: string[][];
+      feedbacks: IFeedback[];
+      feedbackBoard: string[][];
+    }[]
+  ) => void;
+  setPrintFeedbacks: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const PrinterConfigPanel: React.FC<Props> = ({ handlePrint, setBoardsToPrintArray, setPrintFeedbacks }) => {
+  // Variables
+  const gameBoard = SearchWordsGame();
+  const gameState = useSelector((state: RootState) => state.game);
+  const themes = gameState.themes;
+  const themesTitles = Object.keys(themes);
+  const minCustomWordsLength = 5;
+
+  // Session Storage
+  const [printSession, setPrintSession] = useSessionStorage('printConfig', {
+    useCustom: false,
+    themesToLoad: [themesTitles[0]],
+    customWords: [],
+    columns: 15,
+    rows: 15,
+    numberOfWords: 20,
+    numberOfBoards: 1,
+    showFeedbacks: false,
+  });
+
+  // States
+  const [useCustom, setUseCustom] = useState(printSession.useCustom);
+  const [themesToLoad, setThemesToLoad] = useState<string[]>(printSession.themesToLoad);
+  const [customWords, setCustomWords] = useState<string[]>(printSession.customWords);
+  const [columns, setColumns] = useState(printSession.columns);
+  const [rows, setRows] = useState(printSession.rows);
+  const [numberOfWords, setNumberOfWords] = useState(printSession.numberOfWords);
+  const [numberOfBoards, setNumberOfBoards] = useState(printSession.numberOfBoards);
+  const [words, setWords] = useState<string[]>(themes[themesTitles[0]]);
+  const [showFeedbacks, setShowFeedbacks] = useState(printSession.showFeedbacks);
+  setPrintFeedbacks(printSession.showFeedbacks);
+
+  // Functions
+  const handleCustomWordListChanges = (wordsList: string[]) => {
+    setCustomWords(wordsList);
+    printSession.customWords = wordsList;
+    if (wordsList.length < minCustomWordsLength) {
+      setUseCustom(false);
+      printSession.useCustom = false;
+      if (themesToLoad.length === 0) {
+        setThemesToLoad([themesTitles[0]]);
+        printSession.themesToLoad = [themesTitles[0]];
+      }
+    }
+    setPrintSession(printSession);
+  };
+  const handleThemesToLoad = (target: HTMLInputElement) => {
+    let newThemesToLoad = [];
+    if (target.checked) {
+      newThemesToLoad = [...themesToLoad, target.value];
+    } else {
+      newThemesToLoad = themesToLoad.filter((theme) => {
+        return theme !== target.value;
+      });
+    }
+    setThemesToLoad(newThemesToLoad);
+    setPrintSession({ ...printSession, themesToLoad: newThemesToLoad });
+  };
+  const generateBoardsToPrint = () => {
+    const boardsToPrint = [];
+    for (let index = 0; index < numberOfBoards; index++) {
+      const newBoard = gameBoard.getBoard({
+        boardSize: { columns, rows },
+        useCustom: useCustom,
+        customWords,
+        numberOfWords,
+        words,
+      });
+      const newFeedbacks = gameBoard.getFeedbacks();
+      const newFeedbackBoard = gameBoard.getFeedbackBoard();
+      const newBoardToPrint: {
+        board: string[][];
+        feedbacks: IFeedback[];
+        feedbackBoard: string[][];
+      } = { board: newBoard, feedbacks: newFeedbacks, feedbackBoard: newFeedbackBoard };
+      boardsToPrint.push(newBoardToPrint);
+    }
+    setBoardsToPrintArray(boardsToPrint);
+  };
+
+  // Effects
+  useLayoutEffect(() => {
+    const newWords: string[] = [];
+    themesToLoad.map((themeTitle) => newWords.push(...themes[themeTitle]));
+    setWords(newWords);
+  }, [themesToLoad]);
+
+  useLayoutEffect(() => {
+    generateBoardsToPrint();
+  }, [words, columns, rows, numberOfWords, numberOfBoards]);
+
+  useLayoutEffect(() => {
+    if (useCustom && customWords.length >= minCustomWordsLength) {
+      generateBoardsToPrint();
+    }
+  }, [customWords, useCustom]);
+
+  return (
+    <div className="printer-config-board">
+      <div className="sizes-container">
+        <RangeInputComponent
+          name="numOfColumns"
+          labelText="Numero de colunas"
+          min={10}
+          max={40}
+          defaultValue={printSession.columns}
+          setInputValue={(columns: number) => {
+            setColumns(columns);
+            setPrintSession({ ...printSession, columns: columns });
+          }}
+        />
+        <RangeInputComponent
+          name="numOfRows"
+          labelText="Numero de linhas"
+          min={10}
+          max={35}
+          defaultValue={printSession.rows}
+          setInputValue={(rows: number) => {
+            setRows(rows);
+            setPrintSession({ ...printSession, rows: rows });
+          }}
+        />
+      </div>
+      <div className="amount-container">
+        <RangeInputComponent
+          name="numOfWords"
+          labelText="Numero de palavras"
+          min={1}
+          max={70}
+          defaultValue={printSession.numberOfWords}
+          setInputValue={(numOfwords: number) => {
+            setNumberOfWords(numOfwords);
+            setPrintSession({ ...printSession, numberOfWords: numOfwords });
+          }}
+        />
+        <RangeInputComponent
+          name="numOfBoards"
+          labelText="Numero de Quadros"
+          min={1}
+          max={20}
+          defaultValue={printSession.numberOfBoards}
+          setInputValue={(numOfBoards: number) => {
+            setNumberOfBoards(numOfBoards);
+            setPrintSession({ ...printSession, numberOfBoards: numOfBoards });
+          }}
+        />
+      </div>
+      <div className="custom-words-container">
+        <CustomWordListConfig
+          useCustom={useCustom}
+          handleUseCustomChanges={(checked) => {
+            setUseCustom(checked);
+            setPrintSession({ ...printSession, useCustom: checked });
+          }}
+          customWordList={printSession.customWords}
+          handleCustomWordListChanges={handleCustomWordListChanges}
+          loadThemesLength={themesToLoad.length}
+          cols={10}
+          rows={6}
+          minWords={minCustomWordsLength}
+        />
+      </div>
+      <div className="themes-container">
+        <ThemesSelector
+          themes={themesTitles}
+          loadThemes={printSession.themesToLoad}
+          useCustom={printSession.useCustom}
+          handleLoadThemes={handleThemesToLoad}
+        />
+      </div>
+      <div className="action-btns">
+        <label htmlFor="answers-checkbox">
+          <input
+            type="checkbox"
+            checked={showFeedbacks}
+            onChange={() => {
+              setPrintFeedbacks(!showFeedbacks);
+              setShowFeedbacks(!showFeedbacks);
+              setPrintSession({ ...printSession, showFeedbacks: !showFeedbacks });
+            }}
+            name="withAnswers"
+            id="answers-checkbox"
+          />
+          <button className="toggle-button">Incluir gabarito</button>
+        </label>
+        <button className="toggle-button" onClick={handlePrint}>
+          Imprimir
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PrinterConfigPanel;
--- src/components/printer/index.tsx
+++ src/components/printer/index.tsx
@@ -1,222 +1,35 @@
 import './style.scss';
 import './style-mobile.scss';
 
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Board from '../board';
-import CustomWordListConfig from '../configCustomWordList';
 import FeedbackBoard from '../feedbackBoard';
 import IFeedback from '../../types/feedback';
-import RangeInputComponent from '../rangeInputComponent';
-import { RootState } from '../../app/store';
-import SearchWordsGame from '../../utils/SearchWordGame';
-import ThemesSelector from '../configThemesSelector';
+import PrinterConfigPanel from './configPanel';
 import WordList from '../feedbackWordList';
 import { useReactToPrint } from 'react-to-print';
-import { useSelector } from 'react-redux';
-import useSessionStorage from '../../utils/customHooks/useSessionStorage';
 
 const Printer: React.FC = () => {
-  // Variables
-  const gameBoard = SearchWordsGame();
-  const gameState = useSelector((state: RootState) => state.game);
-  const themes = gameState.themes;
-  const themesTitles = Object.keys(themes);
-  const minCustomWordsLength = 5;
-
-  // Session Storage
-  const [printSession, setPrintSession] = useSessionStorage('printConfig', {
-    useCustom: false,
-    themesToLoad: [themesTitles[0]],
-    customWords: [],
-    columns: 15,
-    rows: 15,
-    numberOfWords: 20,
-    numberOfBoards: 1,
-    showFeedbacks: false,
-  });
-
-  // States
-  const [useCustom, setUseCustom] = useState(printSession.useCustom);
-  const [themesToLoad, setThemesToLoad] = useState<string[]>(printSession.themesToLoad);
-  const [customWords, setCustomWords] = useState<string[]>(printSession.customWords);
-  const [columns, setColumns] = useState(printSession.columns);
-  const [rows, setRows] = useState(printSession.rows);
-  const [numberOfWords, setNumberOfWords] = useState(printSession.numberOfWords);
-  const [numberOfBoards, setNumberOfBoards] = useState(printSession.numberOfBoards);
-  const [words, setWords] = useState<string[]>(themes[themesTitles[0]]);
+  const [printFeedbacks, setPrintFeedbacks] = useState(false);
   const [boardsToPrintArray, setBoardsToPrintArray] = useState<
     { board: string[][]; feedbacks: IFeedback[]; feedbackBoard: string[][] }[]
   >([]);
-  const [showFeedbacks, setShowFeedbacks] = useState(printSession.showFeedbacks);
 
   // References
   const componentRef = useRef<HTMLDivElement>(null);
 
-  // Functions
-  const handleCustomWordListChanges = (wordsList: string[]) => {
-    setCustomWords(wordsList);
-    printSession.customWords = wordsList;
-    if (wordsList.length < minCustomWordsLength) {
-      setUseCustom(false);
-      printSession.useCustom = false;
-      if (themesToLoad.length === 0) {
-        setThemesToLoad([themesTitles[0]]);
-        printSession.themesToLoad = [themesTitles[0]];
-      }
-    }
-    setPrintSession(printSession);
-  };
-  const handleThemesToLoad = (target: HTMLInputElement) => {
-    let newThemesToLoad = [];
-    if (target.checked) {
-      newThemesToLoad = [...themesToLoad, target.value];
-    } else {
-      newThemesToLoad = themesToLoad.filter((theme) => {
-        return theme !== target.value;
-      });
-    }
-    setThemesToLoad(newThemesToLoad);
-    setPrintSession({ ...printSession, themesToLoad: newThemesToLoad });
-  };
-  const generateBoardsToPrint = () => {
-    const boardsToPrint = [];
-    for (let index = 0; index < numberOfBoards; index++) {
-      const newBoard = gameBoard.getBoard({
-        boardSize: { columns, rows },
-        useCustom: useCustom,
-        customWords,
-        numberOfWords,
-        words,
-      });
-      const newFeedbacks = gameBoard.getFeedbacks();
-      const newFeedbackBoard = gameBoard.getFeedbackBoard();
-      const newBoardToPrint: {
-        board: string[][];
-        feedbacks: IFeedback[];
-        feedbackBoard: string[][];
-      } = { board: newBoard, feedbacks: newFeedbacks, feedbackBoard: newFeedbackBoard };
-      boardsToPrint.push(newBoardToPrint);
-    }
-    setBoardsToPrintArray(boardsToPrint);
-  };
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
 
-  // Effects
-  useLayoutEffect(() => {
-    const newWords: string[] = [];
-    themesToLoad.map((themeTitle) => newWords.push(...themes[themeTitle]));
-    setWords(newWords);
-  }, [themesToLoad]);
-
-  useLayoutEffect(() => {
-    generateBoardsToPrint();
-  }, [words, columns, rows, numberOfWords, numberOfBoards]);
-
-  useLayoutEffect(() => {
-    if (useCustom && customWords.length >= minCustomWordsLength) {
-      generateBoardsToPrint();
-    }
-  }, [customWords, useCustom]);
-
   return (
     <div className="printer-component" ref={componentRef}>
-      <div className="Aqui">
-        <div>Aqui</div>
-      </div>
-      <div className="printer-config-board">
-        <div className="sizes-container">
-          <RangeInputComponent
-            name="numOfColumns"
-            labelText="Numero de colunas"
-            min={10}
-            max={40}
-            defaultValue={printSession.columns}
-            setInputValue={(columns: number) => {
-              setColumns(columns);
-              setPrintSession({ ...printSession, columns: columns });
-            }}
-          />
-          <RangeInputComponent
-            name="numOfRows"
-            labelText="Numero de linhas"
-            min={10}
-            max={35}
-            defaultValue={printSession.rows}
-            setInputValue={(rows: number) => {
-              setRows(rows);
-              setPrintSession({ ...printSession, rows: rows });
-            }}
-          />
-        </div>
-        <div className="amount-container">
-          <RangeInputComponent
-            name="numOfWords"
-            labelText="Numero de palavras"
-            min={1}
-            max={70}
-            defaultValue={printSession.numberOfWords}
-            setInputValue={(numOfwords: number) => {
-              setNumberOfWords(numOfwords);
-              setPrintSession({ ...printSession, numberOfWords: numOfwords });
-            }}
-          />
-          <RangeInputComponent
-            name="numOfBoards"
-            labelText="Numero de Quadros"
-            min={1}
-            max={20}
-            defaultValue={printSession.numberOfBoards}
-            setInputValue={(numOfBoards: number) => {
-              setNumberOfBoards(numOfBoards);
-              setPrintSession({ ...printSession, numberOfBoards: numOfBoards });
-            }}
-          />
-        </div>
-        <div className="custom-words-container">
-          <CustomWordListConfig
-            useCustom={useCustom}
-            handleUseCustomChanges={(checked) => {
-              setUseCustom(checked);
-              setPrintSession({ ...printSession, useCustom: checked });
-            }}
-            customWordList={printSession.customWords}
-            handleCustomWordListChanges={handleCustomWordListChanges}
-            loadThemesLength={themesToLoad.length}
-            cols={10}
-            rows={6}
-            minWords={minCustomWordsLength}
-          />
-        </div>
-        <div className="themes-container">
-          <ThemesSelector
-            themes={themesTitles}
-            loadThemes={printSession.themesToLoad}
-            useCustom={printSession.useCustom}
-            handleLoadThemes={handleThemesToLoad}
-          />
-        </div>
-        <div className="action-btns">
-          <label htmlFor="answers-checkbox">
-            <input
-              type="checkbox"
-              checked={showFeedbacks}
-              onChange={() => {
-                setShowFeedbacks(!showFeedbacks);
-                setPrintSession({ ...printSession, showFeedbacks: !showFeedbacks });
-              }}
-              name="withAnswers"
-              id="answers-checkbox"
-            />
-            <button className="toggle-button">Incluir gabarito</button>
-          </label>
-          <button className="toggle-button" onClick={handlePrint}>
-            Imprimir
-          </button>
-        </div>
-      </div>
+      <PrinterConfigPanel
+        handlePrint={handlePrint}
+        setBoardsToPrintArray={setBoardsToPrintArray}
+        setPrintFeedbacks={setPrintFeedbacks}
+      />
       <div className="boards-to-print">
         {boardsToPrintArray.map((newBoard, i) => {
           return (
@@ -229,7 +42,7 @@ const Printer: React.FC = () => {
               </div>
               {/* .feedback-board is generating unique key prop error */}
               <div
-                className={`feedback-board page-break ${showFeedbacks ? 'include-feedback' : ''}`}
+                className={`feedback-board page-break ${printFeedbacks ? 'include-feedback' : ''}`}
                 key={'f' + i + JSON.stringify(newBoard.feedbacks)}
               >
                 <FeedbackBoard board={newBoard.feedbackBoard} />
