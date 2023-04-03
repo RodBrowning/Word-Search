2eda3a7512cc453223d59a62512021979749c288
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-04-03 03:15:12 -0300

    Improve the layout of menu buttons and optimize the mobile SASS in the game panel

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -45,6 +45,7 @@ const Game: React.FC = () => {
 
   useEffect(() => {
     if (gameState.gameEnded) {
+      // ! Reset matches to 1
       alert('Ended');
     }
     if (gameState.boardData.matchEnded) {
@@ -100,7 +101,7 @@ const Game: React.FC = () => {
               setIsFeedbacksHidden(!isFeedbacksHidden);
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 20 20">
               <path
                 fill="#000"
                 d="m12.81 4.36l-1.77 1.78a4 4 0 0 0-4.9 4.9l-2.76 2.75C2.06 12.79.96 11.49.2 10a11 11 0 0 1 12.6-5.64zm3.8 1.85c1.33 1 2.43 2.3 3.2 3.79a11 11 0 0 1-12.62 5.64l1.77-1.78a4 4 0 0 0 4.9-4.9l2.76-2.75zm-.25-3.99l1.42 1.42L3.64 17.78l-1.42-1.42L16.36 2.22z"
