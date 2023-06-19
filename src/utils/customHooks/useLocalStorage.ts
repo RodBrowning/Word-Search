08a8984d8cf061311211ca99787cc77e0fb54af4
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-06-18 22:43:39 -0300

    Fix: Local storage hook func declaration

--- src/utils/customHooks/useLocalStorage.ts
+++ src/utils/customHooks/useLocalStorage.ts
@@ -3,18 +3,6 @@ import { Dispatch, SetStateAction, useState } from 'react';
 type UseLocalStorageReturnType<T> = [T, Dispatch<T>, boolean, Error | null];
 
 const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorageReturnType<T> => {
-  const [value, setValue] = useState<T>(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : setLocalStorageValue(initialValue);
-    } catch (error) {
-      console.error(error);
-      return initialValue;
-    }
-  });
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<Error | null>(null);
-
   const setLocalStorageValue: Dispatch<T> = (newValue: T) => {
     setIsLoading(true);
     try {
@@ -29,6 +17,18 @@ const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorageReturn
       setError(error as SetStateAction<Error | null>);
     }
   };
+
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : setLocalStorageValue(initialValue);
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
   return [value, setLocalStorageValue, isLoading, error];
 };
